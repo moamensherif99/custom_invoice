42581efa4df07d64f6fbc956f27c6e38826aa51f
Moamen Sherif Abdelkader <moamensherif19@gmail.com>
2025-04-27 18:27:44 +0300

    new

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Custom Invoice',
+    'author': 'Moamen Sherif Abdelkader',
+    'version': '1.0',
+    'depends': ['account'],
+    'data': [
+        "reports/invoice_report.xml"
+    ],
+    'installable': True,
+    'application': False,
+}
